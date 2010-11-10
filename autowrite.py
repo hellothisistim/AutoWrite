c00e63e3d8add577fa8a14a5e9d629655909b3ce
Tim BOWMAN <puffy@netherlogic.com>
2010-11-09 23:16:31 -0500

    progress

--- autowrite.py
+++ autowrite.py
@@ -2,13 +2,18 @@
 w= nuke.createNode('Write', inpanel=False)
 # Rename it to AutoWrite
 # (Also, deal with the number problem)
-w.knob('name').setValue("Auto" + w.knob('name').value())
+count = 1
+while nuke.exists('AutoWrite' + str(count)):
+    count += 1
+w.knob('name').setValue('AutoWrite' + str(count))
 
 # Add the tab to hold the variables containing path fragments so we can have a less messy file path.
 t = nuke.Tab_Knob("Path Fragments")
 w.addKnob(t)
-w.addKnob(nuke.EvalString_Knob('proj_root', 'Project Root', '[value root.name]'))
-
+w.addKnob(nuke.EvalString_Knob('proj_root', 'Project Root', '[join [lrange [split [value root.name] / ] 0 4 ] / ]'))
+w.addKnob(nuke.EvalString_Knob('seq', 'Sequence', ''))
+w.addKnob(nuke.EvalString_Knob('shot', 'Shot Name', ''))
+w.addKnob(nuke.EvalString_Knob('script', 'Script Name', '[file tail [value root.name] ]'))
 
 
 
